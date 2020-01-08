90d51023d8e844b8c320cc71ac44803380a4923f
jerry <jerry@jerrys-MacBook-Pro.local>
2020-01-08 11:52:20 -0700

    Add iteration 3 100%

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -6,5 +6,23 @@ class Recipe
 		@name = name
 		@ingredients_required = {}
 	end
+	
+	def add_ingredient(ingredient, number)
+		@ingredients_required[ingredient] = number
+	end
 
-end
+	def amount_required(ingredient)
+		@ingredients_required[ingredient]
+	end
+
+	def ingredients
+		@ingredients_required.keys
+	end
+
+	def total_calories
+		ingredients_required.sum {|ingredient, number| ingredient.calories * number}
+	end
+				
+
+
+end	
--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -14,7 +14,32 @@ class IngredientTest < Minitest::Test
 
 	def test_its_variables
 		assert_equal "Mac and Cheese", @recipe.name
-		assert_equal {}, @recipe.ingredients_required
+		assert_equal({}, @recipe.ingredients_required)
+	end
+
+	def test_add_ingredient
+		@recipe.add_ingredient(@ingredient1, 2)
+		@recipe.add_ingredient(@ingredient2, 8)
+		assert_includes  @recipe.ingredients_required.keys, @ingredient1
+	end
+
+	def test_amount_required
+		@recipe.add_ingredient(@ingredient1, 2)
+		@recipe.add_ingredient(@ingredient2, 8)
+		assert_equal 2, @recipe.amount_required(@ingredient1)
+		assert_equal 8, @recipe.amount_required(@ingredient2)
+	end
+
+	def test_ingredients
+		@recipe.add_ingredient(@ingredient1, 2)
+		@recipe.add_ingredient(@ingredient2, 8)
+		assert_includes @recipe.ingredients, @ingredient2
+	end
+
+	def test_total_calores
+		@recipe.add_ingredient(@ingredient1, 2)
+		@recipe.add_ingredient(@ingredient2, 8)
+		assert_equal 440, @recipe.total_calories
 	end
 		
 end
