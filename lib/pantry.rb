5bb9aaa8562366f5b554292dd3f43d9bc5a7008b
jerry <jerry@jerrys-MacBook-Pro.local>
2020-01-08 12:35:09 -0700

    Add iteration 3 100%

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -5,5 +5,37 @@ class Pantry
 	def initialize
 		@stock = {}
 	end
+	
+	def stock_check(ingredient)
+		if stock[ingredient]
+			stock[ingredient]
+		else
+			0
+		end
+	end
+
+	def restock(ingredient, number)
+		if stock[ingredient]
+			stock[ingredient] += number
+		else
+			stock[ingredient] = number
+		end
+	end
+
+	def enough_ingredients_for?(recipe)
+		if (recipe.ingredients_required.keys - stock.keys != [])
+			return false
+		else
+			compared_stock = stock.map {|ingredient, number| number - recipe.ingredients_required[ingredient]}
+			binding.pry
+			if compared_stock.min < 0
+				binding.pry
+				return false
+			else
+				return true
+			end
+		end
+				
+	end
 
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -19,5 +19,26 @@ class PantryTest < Minitest::Test
 	def test_its_variables
 		assert_equal({}, @pantry.stock)
 	end
+	
+
+	def test_stock_check_restock
+		assert_equal 0, @pantry.stock_check(@ingredient1)
+		@pantry.restock(@ingredient1, 5)
+		@pantry.restock(@ingredient1, 10)
+		assert_equal 15, @pantry.stock_check(@ingredient1)		
+	end
+
+	def test_enough_ingredients_for?
+		@pantry.restock(@ingredient1, 5)
+		@pantry.restock(@ingredient1, 10)
+		assert_equal false, @pantry.enough_ingredients_for?(@recipe)
+		@pantry.restock(@ingredient2, 7)
+		assert_equal false, @pantry.enough_ingredients_for?(@recipe)
+		@pantry.restock(@ingredient2, 1)
+		binding.pry
+		assert_equal true, @pantry.enough_ingredients_for?(@recipe)
+	end
+	
 
 end
+
